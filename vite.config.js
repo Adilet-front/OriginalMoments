8c84c83a4e6cbe36a500d3782d87c2c819d4efc6
Adilet <adilettinadilettin@gmail.com>
2026-05-19 10:23:31 +0600

    fix: update vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Lending/', // <-- Добавьте эту строку (имя вашего репозитория)
 })
