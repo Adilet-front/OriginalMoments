d206572b8bedea8135d469c82038f39699ccc8e4
Adilet <adilettinadilettin@gmail.com>
2026-05-19 11:20:01 +0600

    Or

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Lending/', // <-- Добавьте эту строку (имя вашего репозитория)
+  base: '/OriginalMoments/', // <-- Добавьте эту строку (имя вашего репозитория)
 })
